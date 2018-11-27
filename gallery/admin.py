a84afa788633f798aea0968c17e527abcafbce59
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2018-11-27 23:42:01 +0200

    Nicer admin interface

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,5 +3,25 @@ from django.contrib import admin
 from .models import Photo
 from .models import Album
 
-admin.site.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'description', 'original']}),
+        ('Metadata', {'fields': ['date_taken',
+                                 'make',
+                                 'model',
+                                 'lens',
+                                 'aperture',
+                                 'focal_length',
+                                 'shut_numer',
+                                 'shut_denom',
+                                 'iso',
+                                 ],
+                      'classes': ['collapse']}),
+        ]
+    list_display = ('name', 'original', 'date_taken')
+    list_filter = ['date_taken']
+    search_fields = ['name', 'original']
+
+admin.site.register(Photo, PhotoAdmin)
+
 admin.site.register(Album)
