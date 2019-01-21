a4a378c7c7fa83f4274cd11eae88fedd6aa63d72
Bernhard Bliem <bernhard.bliem@gmail.com>
2019-01-21 12:23:02 +0200

    Nicer album selection

--- gallery/static/gallery/photo_batch_edit.js
+++ gallery/static/gallery/photo_batch_edit.js
@@ -1,16 +1,16 @@
 var photosInSameAlbums;
 var photosInSameAlbums;
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   // TODO XHR to get list of all albums
-  albumsSelector = $("#select_albums");
+  albumsSelector = $("#select-albums");
   photosInSameAlbums = true;
 
   albumsSelector.prop("disabled", true);
   albumsSelector.blur();
 
-
-  albumsSelector.change(function() {
+  //albumsSelector.change(function() {
+  albumsSelector.on("changed.bs.select", function() {
     // If selected photos are from different albums, ask for confirmation before overriding albums.
     if(!photosInSameAlbums) {
       if(confirm("The selected photos are in different albums. Do you want to override this and put them in the selected albums instead?")) {
@@ -32,7 +32,6 @@ document.addEventListener('DOMContentLoaded', function() {
       $(this).attr("data-albums", "[" + albumsSelector.val() + "]");
 
       // Send AJAX album change request to server
-      console.log("albums: " + albumsSelector.val());
       $.ajax({
         url: $(this).parent().attr("href"), // XXX
         type: "PATCH",
@@ -55,7 +54,7 @@ function openEditSidebar() {
   $("#content").addClass("right-sidebar-shown");
   $("#gallery>a").addClass("disabled");
 
-  $('#gallery').justifiedGallery({
+  $("#gallery").justifiedGallery({
     rowHeight: 120,
     margins: 12,
   }).selectable({
@@ -67,6 +66,8 @@ function openEditSidebar() {
         albumsSelector.val([]);
         albumsSelector.prop("disabled", true);
         albumsSelector.blur();
+        albumsSelector.selectpicker({title: "No photos selected"});
+        albumsSelector.selectpicker("render")
         return;
       }
       albumsSelector.prop("disabled", false);
@@ -90,20 +91,45 @@ function openEditSidebar() {
       if(photosInSameAlbums) {
         // Select the (equal) albums of selected photos in the album picker.
         albumsSelector.val(JSON.parse(firstAlbumList));
+        albumsSelector.selectpicker({title: "Select albums"});
       }
       else {
         albumsSelector.val([]);
+        albumsSelector.selectpicker({title: "Photos in different albums"});
       }
+      albumsSelector.selectpicker("refresh").selectpicker("render");
     }
   });
 }
 
+function initAlbumsSelector() {
+  albumsSelector = $("#select-albums");
+  albumsSelector.selectpicker({title: "No photos selected"});
+  albumsSelector.val([]);
+  albumsSelector.selectpicker("render");
+}
+
 function closeEditSidebar() {
+  initAlbumsSelector();
   $("#content").removeClass("right-sidebar-shown");
   $("#gallery>a").removeClass("disabled");
 
-  $('#gallery').justifiedGallery({
+  $("#gallery").justifiedGallery({
     rowHeight: jgRowHeight,
     margins: 3,
   }).selectable("destroy");
+}
+
+function selectAllPhotos() {
+  // https://stackoverflow.com/questions/3140017/how-to-programmatically-select-selectables-with-jquery-ui
+  $("a.ui-selectee").addClass("ui-selecting");
+  // trigger the mouse stop event (this will select all .ui-selecting elements, and deselect all .ui-unselecting elements)
+  $("#gallery").data("ui-selectable")._mouseStop(null);
+}
+
+function deselectAllPhotos() {
+  // https://stackoverflow.com/questions/3140017/how-to-programmatically-select-selectables-with-jquery-ui
+  $("a.ui-selectee").removeClass("ui-selected").addClass("ui-unselecting");
+  // trigger the mouse stop event (this will select all .ui-selecting elements, and deselect all .ui-unselecting elements)
+  $("#gallery").data("ui-selectable")._mouseStop(null);
 }
