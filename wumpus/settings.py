1fe53dcd09a2a09fe8fbccd8c1a246e7330558b3
Bernhard Bliem <bernhard.bliem@gmail.com>
2019-01-26 12:47:16 +0200

    Production database settings

--- wumpus/settings.py
+++ wumpus/settings.py
@@ -88,12 +88,24 @@ WSGI_APPLICATION = 'wumpus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': decouple.config('DB_NAME'),
+            'USER': decouple.config('DB_USER'),
+            'PASSWORD': decouple.config('DB_PASSWORD'),
+            'HOST': decouple.config('DB_HOST'),
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
