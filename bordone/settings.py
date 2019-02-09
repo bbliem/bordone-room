2f5b68cbebfb8795d3837cc08de0fdbaa86e4e87
Bernhard Bliem <bernhard.bliem@gmail.com>
2019-02-09 15:44:59 +0200

    Made time zone configurable

--- bordone/settings.py
+++ bordone/settings.py
@@ -167,7 +167,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Europe/Helsinki'
+TIME_ZONE = decouple.config('TIME_ZONE', default='UTC')
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
